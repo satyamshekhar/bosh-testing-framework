ed682cb22de1018c2dbeef25d33d0982e914d326
Satyam Shekhar <satyamshekhar@gmail.com>
2012-04-14 00:41:45 +0530

    Adding config file.

---
+++ config.js
@@ -0,0 +1,7 @@
+exports.lookup = {
+    port: 9000,
+    default: "localhost:5222",
+    "fb-messaging-handler" : "localhost:5222",
+    "tfd-messaging-handler": "localhost:5222",
+    "gtalk-messaging-handler": "localhost:5222"
+};

9b6289e0da3f831f2324ba85ac71981ab18d442d
Satyam Shekhar <satyamshekhar@gmail.com>
2012-04-16 10:58:13 +0530

    indentation fixes

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 exports.lookup = {
     port: 9000,
-    default: "localhost:5222",
-    "fb-messaging-handler" : "localhost:5222",
-    "tfd-messaging-handler": "localhost:5222",
+    "fb-messaging-handler"   : "localhost:5222",
+    "tfd-messaging-handler"  : "localhost:5222",
     "gtalk-messaging-handler": "localhost:5222"
 };

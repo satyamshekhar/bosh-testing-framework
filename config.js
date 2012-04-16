1b3ac1e116a177b55d717483964b455c308ec3b3
Satyam Shekhar <satyamshekhar@gmail.com>
2012-04-16 11:57:05 +0530

    Adding config for xmpp-sever

--- config.js
+++ config.js
@@ -4,3 +4,16 @@ exports.lookup = {
     "tfd-messaging-handler"  : "localhost:5222",
     "gtalk-messaging-handler": "localhost:5222"
 };
+
+exports.xmppServer = {
+    port: 3663,
+    packetHandlers: [
+        require("./lib/xmpp-server/packet-handler/roster.js"),
+        require("./lib/xmpp-server/packet-handler/presence.js"),
+        require("./lib/xmpp-server/packet-handler/message.js")
+    ]
+};
+
+exports.client = {
+    
+};
